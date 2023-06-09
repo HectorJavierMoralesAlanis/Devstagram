12a29bb76131b9ba2cf0d7ea83e8978ba11cbbff
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-08 20:16:10 -0600

    Version Final

--- Devstagram/resources/views/layouts/app.blade.php
+++ Devstagram/resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
         <title>@yield('titulo')</title>
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
+        <!-- Insertar estilo de dropzone-->
+        <link href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" rel="stylesheet" type="text/css" />
     </head>
     <body class="bg-[#1c1917]">
         <!-- Encabezado de la pagina-->
